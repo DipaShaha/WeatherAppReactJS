e25187cc34021e8fef2359a54148986de09ea201
DipaShaha <dipashaha92@gmail.com>
2019-05-23 03:38:27 +0600

    completed Add Item with message

--- src/App.js
+++ src/App.js
@@ -6,23 +6,37 @@ export class App extends React.Component {
   constructor(props){
     super(props);
     this.state={
-      buyItems:['milk','bread','fruit']
+      buyItems:['milk','bread','fruit'],
+      message:''
     }
   }
+
   onAddItem=(event)=>{
     event.preventDefault();
     const {buyItems}=this.state;
     const newItem=this.newItem.value;
-    this.setState({
-      buyItems:[...this.state.buyItems,newItem]
-    })
+    const isOnTheList=buyItems.includes(newItem);
+    if(isOnTheList){
+        this.setState({message:'this item is already on the list'})
+    }else{
+       newItem!=='' && this.setState({
+        buyItems:[...this.state.buyItems,newItem],
+        message:''
+      })
+
+    }
+   
     this.addForm.reset();
 
   }
 
+  onRemoveItem(){
+    
+  }
+
 
   render(){
-    const {buyItems}=this.state; //return r baire
+    const {buyItems,message}=this.state; //return r baire
     return (
       <div className="App">
         <h1>Shopping List</h1>
@@ -41,30 +55,34 @@ export class App extends React.Component {
             <button type="submit" className="btn btn-primary mb-1">Add</button>
           </form>
 
+        <div className="content">
+           {
 
-
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Item</th>
-              <th scope="col">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-          {buyItems.map(item=>{
-              return(
-              <tr key={item}>
-                <th scope="row">1</th>
-                <td>{item}</td>
-                <td>Otto</td>
-              </tr>
-              );
-           })
+            message!=='' && <p className="message text-danger">{message}</p>
           }
-            
-          </tbody>
-        </table>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Item</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+            {buyItems.map(item=>{
+                return(
+                <tr key={item}>
+                  <th scope="row">1</th>
+                  <td>{item}</td>
+                  <td>Otto</td>
+                </tr>
+                );
+             })
+            }
+              
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
